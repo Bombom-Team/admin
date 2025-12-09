4b50f7b2ebc5de924afe2ee85e806a82d64c93a6
ChoiDongjun <adong0808@naver.com>
2025-12-09 14:51:10 +0900

    fix: patch로 수정

--- src/main/java/news/bombomadmin/member/controller/MemberController.java
+++ src/main/java/news/bombomadmin/member/controller/MemberController.java
@@ -7,7 +7,7 @@ import news.bombomadmin.member.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,7 +21,7 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @PutMapping("/{id}/role")
+    @PatchMapping("/{id}/role")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateRole(
             @PathVariable @Positive(message = "id는 1 이상이어야 합니다.") Long id,
