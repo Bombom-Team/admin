8a02365af2c2c6b0f8c0b9f2febe0318a3642959
ChoiDongjun <adong0808@naver.com>
2025-12-15 23:03:54 +0900

    feat: 공지사항 포스팅 API 추가

---
+++ src/main/java/news/bombomadmin/notice/controller/NoticeController.java
@@ -0,0 +1,22 @@
+package news.bombomadmin.notice.controller;
+
+import lombok.RequiredArgsConstructor;
+import news.bombomadmin.notice.dto.CreateNoticeRequest;
+import news.bombomadmin.notice.service.NoticeService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/notices")
+public class NoticeController {
+
+    private final NoticeService noticeService;
+
+    @PostMapping
+    public void createNotice(@RequestBody CreateNoticeRequest request) {
+        noticeService.createNotice(request);
+    }
+}
