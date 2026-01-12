ee5bc5ad67312609ae76f979b64b0278e7416f81
ChoiDongjun <adong0808@naver.com>
2026-01-12 16:24:03 +0900

    fix: s3 업로드 후 반환하는 url 수정

--- src/main/java/me/bombom/api/v1/file/service/S3FileService.java
+++ src/main/java/me/bombom/api/v1/file/service/S3FileService.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import me.bombom.api.v1.common.exception.CServerErrorException;
 import me.bombom.api.v1.common.exception.ErrorContextKeys;
@@ -14,6 +15,7 @@ import me.bombom.api.v1.common.exception.ErrorDetail;
 import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -29,22 +31,40 @@ public class S3FileService {
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucketName;
 
+    @Value("${spring.cloud.aws.cloudfront.domain:}")
+    private String cloudFrontDomain;
+
     public String upload(MultipartFile file) {
-        String originalFileName = file.getOriginalFilename();
-        String storeFileName = createStoreFileName(originalFileName);
+        String storeFileName = createStoreFileName(file);
 
         try (InputStream inputStream = file.getInputStream()) {
             InputStream uploadStream = inputStream;
+
             if (isImage(file)) {
                 uploadStream = resizeImage(inputStream);
             }
 
             s3Template.upload(bucketName, storeFileName, uploadStream);
-            return s3Template.download(bucketName, storeFileName)
-                    .getURL().toString();
+            return getFileUrl(storeFileName);
         } catch (IOException e) {
             throw new CServerErrorException(ErrorDetail.EXTERNAL_API_ERROR)
                     .addContext(ErrorContextKeys.OPERATION, "s3Upload");
+        }
+    }
+
+    private String getFileUrl(String storeFileName) {
+        if (StringUtils.hasText(cloudFrontDomain)) {
+            return cloudFrontDomain + "/" + storeFileName;
+        }
+        return getS3Url(storeFileName);
+    }
+
+    private String getS3Url(String storeFileName) {
+        try {
+            return s3Template.download(bucketName, storeFileName).getURL().toString();
+        } catch (IOException e) {
+            throw new CServerErrorException(ErrorDetail.EXTERNAL_API_ERROR)
+                    .addContext(ErrorContextKeys.OPERATION, "s3GetUrl");
         }
     }
 
@@ -62,8 +82,35 @@ public class S3FileService {
         return contentType != null && contentType.startsWith("image/");
     }
 
-    private String createStoreFileName(String originalFilename) {
+    private String createStoreFileName(MultipartFile file) {
+        String ext = extractExt(file);
+        String uuid = UUID.randomUUID().toString();
         String datePath = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMM"));
-        return datePath + "/" + originalFilename;
+        return datePath + "/" + uuid + "." + ext;
+    }
+
+    private String extractExt(MultipartFile file) {
+        String ext = StringUtils.getFilenameExtension(file.getOriginalFilename());
+        if (StringUtils.hasText(ext)) {
+            return ext;
+        }
+        return getExtensionFromContentType(file.getContentType());
+    }
+
+    private String getExtensionFromContentType(String contentType) {
+        if (!StringUtils.hasText(contentType)) {
+            return "jpg";
+        }
+
+        if (contentType.contains("png"))
+            return "png";
+        if (contentType.contains("gif"))
+            return "gif";
+        if (contentType.contains("webp"))
+            return "webp";
+        if (contentType.contains("jpeg"))
+            return "jpg";
+
+        return "jpg";
     }
 }
