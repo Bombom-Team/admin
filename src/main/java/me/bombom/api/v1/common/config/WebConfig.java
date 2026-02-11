bbb29b67e5450e3c40ca75a626ed12f0dce131ae
ChoiDongjun <adong0808@naver.com>
2026-02-11 22:34:10 +0900

    feat: 개발 서버 테스트 API 작성

--- src/main/java/me/bombom/api/v1/common/config/WebConfig.java
+++ src/main/java/me/bombom/api/v1/common/config/WebConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.bombom.api.v1.common.config.datasource.DbSwitchInterceptor;
 import me.bombom.api.v1.common.resolver.LoginMemberArgumentResolver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -14,11 +15,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final DbSwitchInterceptor dbSwitchInterceptor;
-    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(loginMemberArgumentResolver);
+        resolvers.add(new LoginMemberArgumentResolver(activeProfile));
     }
 
     @Override
