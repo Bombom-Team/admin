206e7113c5467232eb029856295b390c51aa54f0
ChoiDongjun <adong0808@naver.com>
2026-02-21 14:26:07 +0900

    test: 실패하는 테스트 수정

--- src/test/java/me/bombom/api/v1/dev/controller/LambdaPlaywrightControllerTest.java
+++ src/test/java/me/bombom/api/v1/dev/controller/LambdaPlaywrightControllerTest.java
@@ -1,31 +1,25 @@
 package me.bombom.api.v1.dev.controller;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import me.bombom.api.v1.common.support.ControllerTestSupport;
 import me.bombom.api.v1.dev.dto.LambdaPlaywrightSourceRequest;
 import me.bombom.api.v1.dev.service.LambdaPlaywrightService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = LambdaPlaywrightController.class)
-class LambdaPlaywrightControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
+class LambdaPlaywrightControllerTest extends ControllerTestSupport {
 
     @MockitoBean
     private LambdaPlaywrightService lambdaPlaywrightService;
@@ -54,10 +48,11 @@ class LambdaPlaywrightControllerTest {
 
         // when & then
         mockMvc.perform(put("/admin/api/v1/lambda-playwright/source")
+                        .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk());
 
-        verify(lambdaPlaywrightService).updateSource(request);
+        verify(lambdaPlaywrightService).updateSource(any(LambdaPlaywrightSourceRequest.class));
     }
 }
