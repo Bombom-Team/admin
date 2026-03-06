fbf899f63fbe190fd9ef16a2d298befd00499b4a
ChoiDongjun <adong0808@naver.com>
2026-03-06 16:33:48 +0900

    refactor: 사용하지 않는 html 파싱 제거

--- src/main/java/me/bombom/api/v1/article/util/html/HtmlTagCleaner.java
+++
@@ -1,6 +0,0 @@
-package me.bombom.api.v1.article.util.html;
-
-public interface HtmlTagCleaner {
-
-    String clean(String html);
-}
--- src/main/java/me/bombom/api/v1/article/util/html/RegexHtmlTagCleaner.java
+++
@@ -1,9 +0,0 @@
-package me.bombom.api.v1.article.util.html;
-
-public class RegexHtmlTagCleaner implements HtmlTagCleaner {
-
-    @Override
-    public String clean(String html) {
-        return html.replaceAll("<[^>]*>", "");
-    }
-}
