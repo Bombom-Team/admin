5dc6e0442de00271c031f62c74c29f9db5c05ae8
rladmstn <oz33177351@gmail.com>
2026-04-05 18:32:16 +0900

    fix: 블로그 글 이미지 등록 시, post 상태에 따른 이미지 상태 등록

--- src/main/java/me/bombom/api/v1/blog/controller/BlogPostController.java
+++ src/main/java/me/bombom/api/v1/blog/controller/BlogPostController.java
@@ -3,14 +3,22 @@ package me.bombom.api.v1.blog.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.bombom.api.v1.blog.dto.UpdateBlogDraftRequest;
+import me.bombom.api.v1.blog.dto.UploadBlogDraftImageResponse;
 import me.bombom.api.v1.blog.service.BlogDraftService;
+import me.bombom.api.v1.blog.service.BlogImageService;
 import me.bombom.api.v1.common.resolver.LoginMember;
 import me.bombom.api.v1.member.domain.Member;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BlogPostController {
 
     private final BlogDraftService blogDraftService;
+    private final BlogImageService blogImageService;
 
     @PutMapping("/{postId}")
     public void updatePost(
@@ -26,5 +35,15 @@ public class BlogPostController {
             @Valid @RequestBody UpdateBlogDraftRequest request
     ) {
         blogDraftService.updatePost(member.getId(), postId, request);
+    }
+
+    @PostMapping(value = "/{postId}/images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public UploadBlogDraftImageResponse uploadPostImage(
+            @LoginMember Member member,
+            @PathVariable Long postId,
+            @RequestPart(value = "imageFile", required = false) MultipartFile imageFile
+    ) {
+        return blogImageService.uploadPostImage(member.getId(), postId, imageFile);
     }
 }
