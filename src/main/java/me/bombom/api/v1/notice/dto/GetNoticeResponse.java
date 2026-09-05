6efe7d95c6140df489d1c1583963aa0ba0ac27ea
minSsan <msp1125@hanyang.ac.kr>
2026-09-05 17:53:16 +0900

    feat: 공지사항 목록 조회 응답 변경

--- src/main/java/me/bombom/api/v1/notice/dto/GetNoticeResponse.java
+++ src/main/java/me/bombom/api/v1/notice/dto/GetNoticeResponse.java
@@ -4,18 +4,27 @@ import com.querydsl.core.annotations.QueryProjection;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import me.bombom.api.v1.notice.domain.NoticeCategory;
+import me.bombom.api.v1.notice.domain.NoticeVisibility;
 
 public record GetNoticeResponse(
                 Long id,
                 String title,
-                String noticeCategory,
+                NoticeCategory noticeCategory,
+                NoticeVisibility visibility,
+                boolean isRepresentative,
                 LocalDate createdAt
 ) {
 
         @QueryProjection
-        public GetNoticeResponse(Long id, String title, String noticeCategory,
-                        LocalDateTime createdAt) {
-                this(id, title, NoticeCategory.valueOf(noticeCategory).getValue(),
+        public GetNoticeResponse(
+                Long id, 
+                String title, 
+                NoticeCategory noticeCategory,
+                NoticeVisibility visibility, 
+                boolean isRepresentative, 
+                LocalDateTime createdAt
+        ) {
+                this(id, title, noticeCategory, visibility, isRepresentative,
                                 createdAt != null ? createdAt.toLocalDate() : null);
         }
 }
