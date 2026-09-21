1df76c08caaaa4462c775e879f8a7a4a08a5a49c
시원 <cnc4750@inha.edu>
2026-09-21 21:17:49 +0900

    feat: s3 이미지 업로드 상황에 대한 실패 로그 추가

--- src/main/java/me/bombom/api/v1/inquiry/service/InquiryImageService.java
+++ src/main/java/me/bombom/api/v1/inquiry/service/InquiryImageService.java
@@ -2,6 +2,7 @@ package me.bombom.api.v1.inquiry.service;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.bombom.api.v1.common.exception.CIllegalArgumentException;
 import me.bombom.api.v1.common.exception.ErrorContextKeys;
 import me.bombom.api.v1.common.exception.ErrorDetail;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class InquiryImageService {
@@ -25,16 +27,28 @@ public class InquiryImageService {
     private String inquiryBucketName;
 
     public InquiryImageUploadResponse uploadImages(List<MultipartFile> images) {
+        log.info("[Inquiry] 이미지 업로드 요청: {}장", images.size());
+
         if (images.size() > MAX_IMAGE_COUNT) {
             throw new CIllegalArgumentException(ErrorDetail.INVALID_INPUT_VALUE)
                     .addContext(ErrorContextKeys.REASON, "이미지는 최대 4장까지 업로드할 수 있습니다.");
         }
 
         List<String> imageUrls = images.stream()
-                .map(image -> s3FileService.uploadToBucketWithMetadata(image, inquiryBucketName, IMAGE_PREFIX))
-                .map(StoredFile::fileUrl)
+                .map(this::uploadImage)
                 .toList();
 
+        log.info("[Inquiry] 이미지 업로드 완료: {}장", imageUrls.size());
         return InquiryImageUploadResponse.from(imageUrls);
+    }
+
+    private String uploadImage(MultipartFile image) {
+        try {
+            StoredFile storedFile = s3FileService.uploadToBucketWithMetadata(image, inquiryBucketName, IMAGE_PREFIX);
+            return storedFile.fileUrl();
+        } catch (RuntimeException e) {
+            log.warn("[Inquiry] 이미지 업로드 실패: filename={}", image.getOriginalFilename(), e);
+            throw e;
+        }
     }
 }
